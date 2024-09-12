aa6b5b3469437784dca3c12d943b5674e55414de
Raziel Sotken <zromik@mail.ru>
2024-09-12 14:31:22 +0400

    Рефакторинг кода функции create_empty_config.

--- survey-bot-app/configure.py
+++ survey-bot-app/configure.py
@@ -4,25 +4,21 @@ from os import path
 
 def create_empty_config():
     config = {
-        'SSB_TOKEN': 'Токен вашего бота',
+        'SSB_TOKEN': '',
         'SSB_DB': '',
-        'SSB_HOST': 'localhost',
-        'SSB_DB_PORT': 'Порт для подключения к БД (можно удалить строку, если порт не изменялся).',
-        'SSB_DB_USER': 'Имя пользователя для подключеня к БД.',
-        'SSB_DB_PASS': 'Пароль для подключения к БД',
-        'SSB_ADM_CMD': 'Команда для получения прав администратора',
-        'SSB_ADM_PWD': 'Пароль администратора',
-        'SSB_SYS_CMD': 'Команда для получения прав супервайзера',
-        'SSB_SYS_PWD': 'Пароль супервайзера',
+        'SSB_ADM_CMD': '',
+        'SSB_ADM_PWD': '',
+        'SSB_SYS_CMD': '',
+        'SSB_SYS_PWD': '',
         'SSB_LOG_PATH': '\logs',
         'SSB_ERR_MAIL': '',
-        'SSB_DB_HOST': 'localhost'
+        'SSB_DB_HOST': ''
     }
     filename = path.join('.env')
     envfile = Path(filename)
     envfile.open(mode="w", encoding="utf-8")
     for key, value in config.items():
-        set_key(envfile, key, value)
+        set_key(envfile, key, value, quote_mode='never')
 
 
 
