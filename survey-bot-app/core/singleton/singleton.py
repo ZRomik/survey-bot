b3a2d2e5d338deddeda74a11aa3e5505eb97f5ba
Raziel Sotken <zromik@mail.ru>
2024-09-13 09:20:42 +0400

    Реализован паттерн "Одиночка".

---
+++ survey-bot-app/core/singleton/singleton.py
@@ -0,0 +1,8 @@
+class Singleton:
+    """
+    Реализация паттерна "Одиночка"
+    """
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls)
+        return cls._instance
